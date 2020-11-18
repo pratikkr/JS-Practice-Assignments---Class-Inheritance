eeb5fe001d3943d5fd9eb5c6d190ab092a0afdab
pratik kumar <kumarpratik861@gmail.com>
2020-11-19 00:22:28 +0530

    changed

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,14 @@
 // import using require
+const shape = require("./shape");
 
 // declare class
-
+class Circle extends shape {
+  constructor() {
+    super();
+    this.color = shape.color;
+  }
+  calculateArea() {}
+}
 
 // export class using module.exports
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,11 @@
 // declare class
+class shape {
+  constructor() {
+    this.color = "green";
+  }
+  drawShape() {}
+  calculateArea() {}
+}
 
 // export class using module.exports
+module.exports = shape;
